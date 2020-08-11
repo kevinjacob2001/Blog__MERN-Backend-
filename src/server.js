ee1002c71b63a9158a04ed0a198c32d4495fd905
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-11 18:18:01 +0530

    added articleInfo json

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-
 const app = express();
+
+const articleInfo = {
+    "learn-react": {
+        upvotes: 0
+    },
+    "learn-node": {
+        upvotes: 0
+    },
+    "my-thoughts-on-resumes": {
+        upvotes: 0
+    }
+}
+
 
 app.use(bodyParser.json())
 
-
-
 app.get('/hello', (req, res) => res.send('Hello!'));
-
-app.get("/hello/:name",(req,res)=>res.send(`Hello ${req.params.name}`))
-
-app.post("/hello",(req,res)=>res.send(`Hello my name is ${req.body.name}, I am ${req.body.age} yrs old`))
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`))
+app.post("/hello", (req, res) => res.send(`Hello my name is ${req.body.name}, I am ${req.body.age} yrs old`))
 
 
 
