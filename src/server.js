2b6c165f87b86ce2783d5d654ca306d582b8bf4e
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-11 22:58:38 +0530

    adding mongo db to Express

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
+import { MongoClient } from 'mongodb';
 
 
 app.use(bodyParser.json())
 
+app.get("/api/articles/:name", async (req, res) => {
+    try{
+        const articleName = req.params.name;
+        const client=await MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true })
+        const db=client.db("my-blog");
+        const articleInfo=await db.collection("articles").findOne({name:articleName})
+        res.status(200).json(articleInfo)
+        client.close();
+    }
+catch(error){
+    res.status(500).json({message:"Error connecting to db",error})
+}
+
+})
+
 app.post("/api/articles/:name/upvote", (req, res) => {
     const articleName = req.params.name;
+
     articleInfo[articleName].upvotes += 1;
     res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes`)
 })
