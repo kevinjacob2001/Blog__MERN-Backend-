785af78ec853c9090a579e83c82bd4e0b87d39ce
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-11 18:08:27 +0530

    Route parameters in EXPRESS

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
-import express, { response } from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 
+app.use(bodyParser.json())
+
+
+
 app.get('/hello', (req, res) => res.send('Hello!'));
 
-app.post("/hello",(req,res)=>res.send("Hello"))
+app.get("/hello/:name",(req,res)=>res.send(`Hello ${req.params.name}`))
+
+app.post("/hello",(req,res)=>res.send(`Hello my name is ${req.body.name}, I am ${req.body.age} yrs old`))
+
+
+
 
 app.listen(8050, () => console.log('Listening on port 8050'));
