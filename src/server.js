2acf573a530df6cc0a5cb530a78ddf8b2970b5c0
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-11 23:01:15 +0530

    Connected the DB to the Express server

--- src/server.js
+++ src/server.js
@@ -7,17 +7,17 @@ import { MongoClient } from 'mongodb';
 app.use(bodyParser.json())
 
 app.get("/api/articles/:name", async (req, res) => {
-    try{
+    try {
         const articleName = req.params.name;
-        const client=await MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true })
-        const db=client.db("my-blog");
-        const articleInfo=await db.collection("articles").findOne({name:articleName})
+        const client = await MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true })
+        const db = client.db("my-blog");
+        const articleInfo = await db.collection("articles").findOne({ name: articleName })
         res.status(200).json(articleInfo)
         client.close();
     }
-catch(error){
-    res.status(500).json({message:"Error connecting to db",error})
-}
+    catch (error) {
+        res.status(500).json({ message: "Error connecting to db", error })
+    }
 
 })
 
