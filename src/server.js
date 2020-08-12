e8b20a9940f4e26fca38141eb8ef8b129aaa247f
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-12 09:38:24 +0530

    updating upvote

--- src/server.js
+++ src/server.js
@@ -21,11 +21,15 @@ app.get("/api/articles/:name", async (req, res) => {
 
 })
 
-app.post("/api/articles/:name/upvote", (req, res) => {
+app.post("/api/articles/:name/upvote", async (req, res) => {
     const articleName = req.params.name;
-
-    articleInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes`)
+    const client = await MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true })
+    const db = client.db("my-blog");
+    const articleName = await db.collection("articles").find({ name: articleName })
+    await db.collection("articles").updateOne({ name: articleName },
+        {"$set": {upvotes: articleInfo.upvotes + 1}})
+    const updatedArticleInfo = await db.collection("articles").find({ name: articleName })
+    res.status(200).json(updatedArticleInfo);
 })
 
 app.post("/api/articles/:name/add-comment", (req, res) => {
