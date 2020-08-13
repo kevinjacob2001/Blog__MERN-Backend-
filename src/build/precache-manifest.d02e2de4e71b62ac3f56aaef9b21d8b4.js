17ba86df6cddad08cbec96311556ba282d690e2d
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-13 11:29:30 +0530

    Completed the BACKEND

---
+++ src/build/precache-manifest.d02e2de4e71b62ac3f56aaef9b21d8b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafdc90c83041cac81c71b9ec1fffb53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b34132f4200bb42c4a",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cac1fcddd90f732d0a9d",
+    "url": "/static/css/main.6cf92cfa.chunk.css"
+  },
+  {
+    "revision": "84b34132f4200bb42c4a",
+    "url": "/static/js/2.6d927d30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d927d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac1fcddd90f732d0a9d",
+    "url": "/static/js/main.851f3bb9.chunk.js"
+  },
+  {
+    "revision": "e11d3e1e52433c2f3408",
+    "url": "/static/js/runtime-main.4ba3e7fd.js"
+  },
+  {
+    "revision": "f0b5850aafa5dc55ada8b539a45c5e76",
+    "url": "/static/media/bg1.f0b5850a.jpg"
+  },
+  {
+    "revision": "ebbd312d3f8bb01a957f57e86e0de6c9",
+    "url": "/static/media/kevin.ebbd312d.jpg"
+  }
+]);
--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
+import path from 'path';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname,'/build')));
 app.use(bodyParser.json());
 
 
@@ -13,16 +15,15 @@ app.get('/api/articles/:name', async (req, res) => {
 
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('my-blog');
-
+    
         const articleInfo = await db.collection('articles').findOne({ name: articleName })
         res.status(200).json(articleInfo);
-
+    
         client.close();
-    } 
-    catch (error) {
+    } catch (error) {
         res.status(500).json({ message: 'Error connecting to db', error });
     }
-
+    
 })
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
@@ -31,11 +32,10 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
 
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('my-blog');
-
+    
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         await db.collection('articles').updateOne({ name: articleName }, {
-            '$set': { upvotes: articleInfo.upvotes + 1 }
-        });
+            '$set': {upvotes: articleInfo.upvotes + 1}});
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(updatedArticleInfo);
         client.close();
@@ -44,24 +44,29 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
     }
 });
 
-app.post('/api/articles/:name/add-comment', async (req, res) => {
-    try {
+app.post('/api/articles/:name/add-comment',async (req, res) => {
+    try{
         const { username, text } = req.body;
         const articleName = req.params.name;
-
+    
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('my-blog');
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
-        await db.collection("articles").updateOne({ name: articleName }, {
-            "$set": { comments: articleInfo.comments.concat({ username, text }) }
+        await db.collection("articles").updateOne({name:articleName},{
+            "$set":{comments:articleInfo.comments.concat({username,text})}
         })
-        const updatedArticleInfo = await db.collection("articles").findOne({ name: articleName })
+        const updatedArticleInfo=await db.collection("articles").findOne({name:articleName})
         res.status(200).json(updatedArticleInfo);
         client.close();
     }
     catch (error) {
         res.status(500).json({ message: 'Error connecting to db', error });
     }
+    
 });
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname+"/build/index.html"))
+})
 
 app.listen(8050, () => console.log('Listening on port 8050'));
